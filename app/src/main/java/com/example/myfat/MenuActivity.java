5a6aaae0ce16eb8f2d949653256f0837bc28d9b1
Egich-17 <chehovich.egor@gmail.com>
2021-03-24 15:14:49 +0200

    added Authentication

--- app/src/main/java/com/example/myfat/MenuActivity.java
+++ app/src/main/java/com/example/myfat/MenuActivity.java
@@ -3,7 +3,11 @@ package com.example.myfat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -13,8 +17,18 @@ public class MenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
         Bundle arguments = getIntent().getExtras();
         String name = arguments.get("name").toString();
+        Button bntExit = findViewById(R.id.btnExit);
 
         TextView tvname= findViewById(R.id.tvName);
         tvname.setText("Привет "+name);
+
+
+
+        bntExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myfat/RegistrationActivity.java
+++ app/src/main/java/com/example/myfat/RegistrationActivity.java
@@ -11,19 +11,33 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.facebook.FacebookSdk;
 
 public class RegistrationActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+
+    private CallbackManager mCallbackManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
         setContentView(R.layout.activity_registration);
+
+
 
         Button btnSignUp = findViewById(R.id.btnReg_reg);
         EditText etEmail = findViewById(R.id.etEmail_reg);
@@ -31,7 +45,8 @@ public class RegistrationActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser user = mAuth.getCurrentUser();
-        Log.d("mydeb", "user  "+user.getUid());
+
+
 
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,5 +77,73 @@ public class RegistrationActivity extends AppCompatActivity {
                          }
         });
 
+        mCallbackManager = CallbackManager.Factory.create();
+        LoginButton loginButton = findViewById(R.id.button_sign_in);
+        loginButton.setReadPermissions("email", "public_profile");
+        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("mydeb", "facebook:onSuccess: ");
+                handleFacebookAccessToken(loginResult.getAccessToken());
+
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d("mydeb", "facebook:onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.d("mydeb", "facebook:onError", error);
+            }
+        });
+
+
     }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d("mydeb", "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("mydeb", "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Log.d("mydeb", "new id user  "+ user.getUid());
+                            Toast.makeText(RegistrationActivity.this,"Successful",Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                            startActivity(intent);
+                        } else {
+
+                            Log.w("mydeb", "signInWithCredential:failure", task.getException());
+                            Toast.makeText(RegistrationActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+    }
+
+
 }
